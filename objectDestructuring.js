aadca30cc39092df496812f5c767eef8180d8a18
Anne Koice M <koice.media@gmail.comgit config --global user.name kicciless>
2024-09-02 15:55:33 +0100

    Add Object Destructuring

---
+++ objectDestructuring.js
@@ -0,0 +1,11 @@
+const student={
+    name:'Sammy'
+    age:16;
+    gender:"Male"
+}
+
+function printStudent( {name, age, gender}, course){
+    console.log(`The student name is ${name}, age is ${age}, gender is ${gener} and course us ${course}`)
+}
+
+printStudent(student,"Computing")
